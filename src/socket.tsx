6630ec814c6af7092772e1cc250187038498c4b6
Valb.Mig <valb.mig@gmail.com>
2024-05-14 09:12:02 -0300

    :bug:(fix): Ajustando captura de informações de usuário por requisição

--- src/socket.tsx
+++ src/socket.tsx
@@ -1,4 +1,4 @@
 "use client";
 
 import { io } from "socket.io-client";
-export const socket = io('ws://172.18.0.2:4000');
+export const socket = io(process.env.NEXT_PUBLIC_WS_SOCKET ? process.env.NEXT_PUBLIC_WS_SOCKET : '');
